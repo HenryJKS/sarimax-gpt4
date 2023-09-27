beff4ef632c099785e2e018d81c5e02155db6bae
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 09:33:17 -0300

    A

--- Script/previsaoDemanda.py
+++ Script/previsaoDemanda.py
@@ -4,10 +4,8 @@ from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
 from statsmodels.tsa.arima.model import ARIMA
-
 from statsmodels.tsa.seasonal import seasonal_decompose
 import statsmodels.api as sm
-from ChatBotWeb.query.queryPrevisaoDemanda import df
 
 pd.set_option('display.max_columns', None)
 
@@ -15,4 +13,16 @@ pd.set_option('display.max_columns', None)
 # SARIMAX é uma extensão do modelo ARIMA para dados não sazonais
 # ARIMA - AutoRegressive Integrated Moving Average
 
-df.to_csv('C:\\Users\\Henry\\Desktop\\dados.csv', index=True)
+df = pd.read_csv('C:\\Users\\logonrmlocal\\Desktop\\dados.csv', sep=',', index_col='ANO')
+
+# usar random forest para prever a demanda de veículos elétricos
+# random forest é um algoritmo de aprendizado supervisionado
+
+# plotar gráfico de linha para visualizar a demanda de veículos elétricos
+df['UNIDADES_VENDIDAS'].plot(figsize=(12, 5), title='Demanda de veículos elétricos', fontsize=14)
+plt.show()
+
+
+# prever UNIDADES_VENDIDAS para 2023
+# usar random forest para prever a demanda de veículos elétricos
+# random forest é um algoritmo de aprendizado supervisionado
